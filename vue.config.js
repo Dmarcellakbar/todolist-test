221a62fd479339a2dbc995944864f47ad2e5a1ba
Dmarcellakbar <dickymarcellino@gmail.com>
2024-09-24 01:14:34 +0700

    pp

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 require('dotenv').config();
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: process.env.VUE_APP_API_URL, // Your backend API URL
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' },
+      },
+    },
+  },
+});
