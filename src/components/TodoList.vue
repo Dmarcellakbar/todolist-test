70e3b111fb24c042570dadc08b61c0dbeb1ad149
Dmarcellakbar <dickymarcellino@gmail.com>
2024-09-24 01:12:36 +0700

    uppp

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -24,7 +24,7 @@ export default {
   methods: {
     async fetchTodos() {
       try {
-        const response = await axios.get(`/todos`);
+        const response = await axios.get(process.env.VUE_APP_API_URL);
         this.todos = response.data;
       } catch (error) {
         console.error('Error fetching todos:', error);
@@ -33,7 +33,7 @@ export default {
     async addTodo() {
       if (this.newTodo.trim() === '') return;
       try {
-        const response = await axios.post(`/todos`, {
+        const response = await axios.post(process.env.VUE_APP_API_URL, {
           title: this.newTodo,
         });
         this.todos.push(response.data);
@@ -44,7 +44,7 @@ export default {
     },
     async deleteTodo(id) {
       try {
-        await axios.delete(`/todos/${id}`);
+        await axios.delete(`${process.env.VUE_APP_API_URL}/${id}`);
         this.todos = this.todos.filter((todo) => todo.id !== id);
       } catch (error) {
         console.error('Error deleting todo:', error);
